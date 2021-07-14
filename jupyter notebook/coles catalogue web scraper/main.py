412035b0c0d957334b186d00c202d25446d62c0a
Tony Nguyen <tony.nguyn.99@gmail.com>
2021-07-15 01:30:09 +1000

    separate sections

--- jupyter notebook/coles catalogue web scraper/main.py
+++ jupyter notebook/coles catalogue web scraper/main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 base_url = 'https://salefinder.com.au/coles-catalogue/coles-catalogue-vic-metro'
 
@@ -9,6 +10,15 @@ headers = {
 
 product_links = []
 
+# 
+
+testlink = 'https://salefinder.com.au/40032/food-and-beverage/groceries/biscuits-and-snacks/carmans-nut-bars-160g175g/400321001/'
+
+
+# # # # # # # # # 
+# # Final Code # #
+# # # # # # # # # 
+
 # for x in range(1, 10):
 #     r = requests.get(f'https://salefinder.com.au/coles-catalogue/coles-catalogue-vic-metro/40032/list?qs={x},,0,0,0')
 #     soup = BeautifulSoup(r.content, 'lxml')
@@ -17,22 +27,45 @@ product_links = []
 #         # print(a['href'])
 #         product_links.append(base_url + '/' + a['href'])
 
-# testlink = 'https://salefinder.com.au/40032/food-and-beverage/groceries/biscuits-and-snacks/carmans-nut-bars-160g175g/400321001/'
+# product_list = []
 
-product_list = []
-for link in product_links:
-    r = requests.get(link, headers = headers)
-    soup = BeautifulSoup(r.content, 'lxml')
+# for link in product_links:
+#     r = requests.get(link, headers = headers)
+#     soup = BeautifulSoup(r.content, 'lxml')
 
-    product_name = soup.find('h1').text.strip()
-    product_price = soup.find('span', {'class': 'price'}).text.strip()
-    product_discount = soup.find('div', {'class': 'price-options'}).text.strip()
+#     product_name = soup.find('h1').text.strip()
+#     product_price = soup.find('span', {'class': 'price'}).text.strip()
+#     product_discount = soup.find('div', {'class': 'price-options'}).text.strip()
 
-    product = {
-        'product name': product_name,
-        'product price': product_price,
-        'product discount': product_discount
-    }
-    product_list.append(product)
+#     product = {
+#         'product name': product_name,
+#         'product price': product_price,
+#         'product discount': product_discount
+#     }
 
-print(product_list)
+#     product_list.append(product)
+
+# df = pd.DataFrame(product_list)
+# print(df.head(15))
+
+
+
+
+
+# # # # # # # # # 
+# # Test Code # #
+# # # # # # # # # 
+r = requests.get(testlink, headers = headers)
+soup = BeautifulSoup(r.content, 'lxml')
+
+product_name = soup.find('h1').text.strip()
+product_price = soup.find('span', {'class': 'price'}).text.strip()
+product_discount = soup.find('div', {'class': 'price-options'}).text.strip()
+
+product = {
+    'product name': product_name,
+    'product price': product_price,
+    'product discount': product_discount
+}
+
+print(product)
